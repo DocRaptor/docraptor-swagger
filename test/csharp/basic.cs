8e6a6cbe2a30799d1efcf556f7436ffb4006e475
Elijah Miller <elijah.miller@gmail.com>
2015-08-01 14:58:41 -0400

    Working c# example.

--- test/csharp/basic.cs
+++ test/csharp/basic.cs
@@ -9,35 +9,23 @@ using DocRaptor.Model;
 using DocRaptor.Api;
 
 
-
 namespace DocRaptorConsoleExample {
   class DocRaptor {
     static void Main(string[] args) {
-      DocRaptor docraptor = new DocRaptor();
+      Configuration.Username = "YOUR_API_KEY_HERE";
+      // Configuration.Debug = true; // Not supported in Csharp
+      DefaultApi docraptor = new DefaultApi();
+
       NewDoc new_doc = new NewDoc();
-      // Doc doc = new Doc();
-      // doc.name = "eli-real-1.pdf";
-      // doc.test = true;
-      // doc.document_content = "hihihi";
-      // doc.document_type = "pdf";
+      Doc doc = new Doc();
+      doc.Name = "eli-real-1.pdf";
+      doc.Test = true;
+      doc.DocumentContent = "hihihi";
+      doc.DocumentType = "pdf";
 
-      // new_doc.doc = doc;
+      new_doc.Doc = doc;
 
-      // docraptor.DocsPost(new_doc, "");
-
-      // DocRaptor::Swagger.configuration.username = "YOUR_API_KEY_HERE";
-      // DocRaptor::Swagger.configuration.debug = true;
-
-      // response = DocRaptor::DefaultApi.docs_post(doc: {test: true, document_content: "hihihi", name: "eli-real-1.pdf", document_type: "pdf"});
-
-      // DocRaptor doc_raptor = new DocRaptor() {
-      //   DocumentUrl = @"http://www.docraptor.com/examples/invoice.html",
-      //   Name = "csharp_sample.pdf",
-      //   Test = true
-      // };
-
-      // doc_raptor.CreatePDF();
-
+      docraptor.DocsPost(new_doc, "");
     }
   }
 }
