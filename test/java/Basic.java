7ee006cf4a4b48da628e2b6f3f47e7050c67aa5b
Elijah Miller <elijah.miller@gmail.com>
2015-08-01 11:39:13 -0400

    2-space indent.

--- test/java/Basic.java
+++ test/java/Basic.java
@@ -3,20 +3,20 @@ import java.net.*;
 import docraptor.*;
 
 public class Basic {
-    public static void main(String[] args) throws Exception{
-        DefaultApi docraptor = new DefaultApi();
-        ApiClient foo = docraptor.getApiClient();
-        foo.setUsername("YOUR_API_KEY_HERE");
-        foo.setDebugging(true);
+  public static void main(String[] args) throws Exception{
+    DefaultApi docraptor = new DefaultApi();
+    ApiClient foo = docraptor.getApiClient();
+    foo.setUsername("YOUR_API_KEY_HERE");
+    foo.setDebugging(true);
 
-        Doc doc = new Doc();
-        NewDoc new_doc = new NewDoc();
-        new_doc.setDoc(doc);
-        doc.setName("eli-real-1.pdf");
-        doc.setDocumentType("pdf");
-        doc.setDocumentContent("hihihi");
-        doc.setTest(true);
-        
-        docraptor.docsPost(new_doc, "YOUR_API_KEY_HERE");
-    }
+    Doc doc = new Doc();
+    NewDoc new_doc = new NewDoc();
+    new_doc.setDoc(doc);
+    doc.setName("eli-real-1.pdf");
+    doc.setDocumentType("pdf");
+    doc.setDocumentContent("hihihi");
+    doc.setTest(true);
+
+    docraptor.docsPost(new_doc, "YOUR_API_KEY_HERE");
+  }
 }
