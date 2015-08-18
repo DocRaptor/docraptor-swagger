179bf096b39e3964cf7a207ed17ad62eb7965625
Elijah Miller <elijah.miller@gmail.com>
2015-08-18 15:41:59 -0400

    Break up Ruby async/sync test and make runnable together.

--- test/ruby/async.rb
+++ test/ruby/async.rb
@@ -2,12 +2,9 @@
 require "bundler/setup"
 Bundler.require
 
-# binding.pry
-
 DocRaptor::Swagger.configuration.username = "YOUR_API_KEY_HERE"
 DocRaptor::Swagger.configuration.debug = true
 
-# response = DocRaptor::DefaultApi.docs_post(test: true, document_content: "<html><body>Swagger Ruby</body></html>", name: "swagger-ruby.pdf", document_type: "pdf")
 response = DocRaptor::DefaultApi.async_docs_post(test: true, document_content: "<html><body>Swagger Ruby</body></html>", name: "swagger-ruby.pdf", document_type: "pdf")
 
 status_response = nil
---
+++ test/ruby/sync.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+require "bundler/setup"
+Bundler.require
+
+DocRaptor::Swagger.configuration.username = "YOUR_API_KEY_HERE"
+DocRaptor::Swagger.configuration.debug = true
+
+response = DocRaptor::DefaultApi.docs_post(test: true, document_content: "<html><body>Swagger Ruby</body></html>", name: "swagger-ruby.pdf", document_type: "pdf")
